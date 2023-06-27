854bedbb79426f57a06dc0e1a1eaf28c152dbadf
frank <fgraupeter@gmail.com>
2023-06-27 21:09:19 +0200

    added librarys

--- esp/MQTT/src/main.cpp
+++ esp/MQTT/src/main.cpp
@@ -1,4 +1,13 @@
+#include <Adafruit_Sensor.h>
 #include <Arduino.h>
+#include <DHT.h>
+#include <ESPmDNS.h>
+#include <PubSubClient.h>
+#include <SPI.h>
+#include <SPIFFS.h>
+#include <WebServer.h>
+#include <WiFi.h>
+#include <esp32-hal-timer.h>
 
 void setup() {
   // put your setup code here, to run once:
