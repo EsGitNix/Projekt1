4ebc3696b698f83bdbe0a5ec306b2a4465e0ac0e
frank <fgraupeter@gmail.com>
2023-06-27 21:26:39 +0200

    implementet screen

--- esp/MQTT/src/main.cpp
+++ esp/MQTT/src/main.cpp
@@ -8,6 +8,39 @@
 #include <WebServer.h>
 #include <WiFi.h>
 #include <esp32-hal-timer.h>
+#include <TFT_eSPI.h>
+
+const char *ssid = "Mesh Igel";        //"ASUSJC";
+const char *password = "rhQ6WbSbE8Rv"; //"123456789";
+
+const int dhtpin = 25;
+DHT dht(dhtpin, DHT22);
+TFT_eSPI tft = TFT_eSPI();
+
+float temperature;
+float humidity;
+
+void showSCrn()
+{
+  temperature = dht.readTemperature();
+  humidity = dht.readHumidity();
+  tft.setCursor(0, 0);
+  tft.setTextSize(3);
+  tft.fillScreen(TFT_BLACK);
+  tft.setTextColor(TFT_GREEN, TFT_BLACK);
+  tft.println("Temp_Humidity");
+  // tft.drawLine(0, 35, 250, 35, TFT_BLUE);
+  tft.println();
+  tft.print(temperature);
+  tft.print(F("C"));
+  tft.print(("_"));
+  tft.print(humidity);
+  tft.print(F("%"));
+  tft.println();
+  tft.println();
+  tft.setTextSize(2);
+  tft.println(localTime());
+}
 
 void setup() {
   // put your setup code here, to run once:
