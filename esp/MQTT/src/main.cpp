8ecee660c30aeb37a49f18e0912e6cb7c12a3085
frank <fgraupeter@gmail.com>
2023-06-28 10:03:57 +0200

    setup display

--- esp/MQTT/src/main.cpp
+++ esp/MQTT/src/main.cpp
@@ -14,11 +14,34 @@ const char *ssid = "Mesh Igel";        //"ASUSJC";
 const char *password = "rhQ6WbSbE8Rv"; //"123456789";
 
 const int dhtpin = 25;
+const int batteryPin = 39;
+
 DHT dht(dhtpin, DHT22);
 TFT_eSPI tft = TFT_eSPI();
 
 float temperature;
 float humidity;
+
+const char *ntpServer = "pool.ntp.org";
+const long gmtOffset_sec = 3600;
+const int daylightOffset_sec = 3600;
+char csvSperator = ';';
+
+float batteryPercantage()
+{
+}
+
+String localTime()
+{
+  char buffer[80];
+  struct tm timeinfo;
+  if (!getLocalTime(&timeinfo))
+  {
+    return ("Failed to obtain time");
+  }
+  strftime(buffer, 80, "%Y-%m-%d %H:%M", &timeinfo);
+  return buffer; //(&timeinfo, "%A, %B %d %Y %H:%M:%S");
+}
 
 void showSCrn()
 {
@@ -42,10 +65,38 @@ void showSCrn()
   tft.println(localTime());
 }
 
-void setup() {
-  // put your setup code here, to run once:
+void setup()
+{
+  Serial.begin(9600);
+  while (!Serial)
+  {
+    ;
+  }
+  pinMode(batteryPin, INPUT);
+
+  int counter = 0;
+  dht.begin();
+  tft.begin();
+  tft.setRotation(1);
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED && counter < 10)
+  {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+    counter++;
+  }
+  Serial.println("Connected");
+  Serial.println(WiFi.localIP());
+
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer); // Set the time server
+  Serial.print("Time set: ");
+  Serial.println(localTime());
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  showSCrn();
+  String dataString = localTime() + csvSperator + temperature + csvSperator + humidity;
+  Serial.println(dataString);
 }
